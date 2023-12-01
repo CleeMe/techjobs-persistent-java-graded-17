0229c4a625f24d130ad564278677b985cad19d76
Christi-Lee <christileecurrier@gmail.com>
2023-12-01 13:50:50 -0600

    added jobs field, getters, setters, M2M annotation mapped by skills

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -10,11 +11,23 @@ public class Skill extends AbstractEntity {
     @Size(max = 500, message = "Description too long!")
    private String description;
 
+    @ManyToMany (mappedBy = "skills")
+    private List<Job> jobs;
+
+
     public Skill()  {}
 
     public String getDescription() {return description;}
 
     public void setDescription(String description)  {this.description = description;}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs (List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 
 }
