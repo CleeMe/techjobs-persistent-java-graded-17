505fd29ab1c2b61f63e94a6ba7fe776aa5015cb5
Christi-Lee <christileecurrier@gmail.com>
2023-12-04 12:20:22 -0600

    commente out @jointable part and used just mapped by in skill and got the same failed test.  Asked TAs for other suggestions/direction

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,6 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
 
@@ -15,11 +13,11 @@ public class Skill extends AbstractEntity {
     private String description;
 
     @ManyToMany(mappedBy = "skills")
-    @JoinTable(
-            name = "job_skill",
-            joinColumns = @JoinColumn(name = "skill_id"),
-            inverseJoinColumns = @JoinColumn(name = "job_id")
-    )
+//    @JoinTable(
+//            name = "job_skill",
+//            joinColumns = @JoinColumn(name = "skill_id"),
+//            inverseJoinColumns = @JoinColumn(name = "job_id")
+//    )
     private List<Job> jobs;
 
     public Skill() {
