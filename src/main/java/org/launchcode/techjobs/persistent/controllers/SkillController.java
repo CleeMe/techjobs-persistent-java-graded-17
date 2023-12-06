5c0a84d81fde0a2860b6679678f259c065cd0cd7
Christi-Lee <christileecurrier@gmail.com>
2023-12-05 19:39:39 -0600

    redoing SkillController.  not passing all tests.  Will work on errors

--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -12,50 +12,42 @@ import java.util.Optional;
 
 
 @Controller
-@RequestMapping("/skills")
+@RequestMapping("skills")
 public class SkillController {
     @Autowired
-    private final SkillRepository skillRepository;
+    private SkillRepository skillRepository;
 
-    @Autowired
-    public SkillController(SkillRepository skillRepository) {
-        this.skillRepository = skillRepository;
-    }
-
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("skills", skillRepository.findAll());
-        return "skills/index";
-    }
-
-    @GetMapping("/add")
+    @GetMapping("add")
     public String displayAddSkillForm(Model model) {
-        model.addAttribute("title", "Add Skill");
-        model.addAttribute(new Skill());
+              model.addAttribute(new Skill());
         return "skills/add";
     }
 
-    @PostMapping("/add")
+    @PostMapping("add")
     public String processAddSkillForm(@ModelAttribute Skill newSkill, Errors errors, Model model) {
         if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Skill");
-            return "skills/add";
+           return "skills/add";
         }
 
         skillRepository.save(newSkill);
         return "redirect:";
     }
 
-    @GetMapping("/view/{skillId}")
-    public String displayViewSkill(Model model, int skillId) {
-        Optional<Skill> optSkill = skillRepository.findById(skillId);
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+        Optional optSkill = skillRepository.findById(skillId);
 
         if (optSkill.isPresent()) {
-            model.addAttribute("skill", optSkill.get());
+            Skill skill = (Skill) optSkill.get();
+            model.addAttribute("skill", skill);
             return "skills/view";
         } else {
-            return "redirect:/skills";
+            return "redirect:../";
         }
+    }
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
     }
 }
